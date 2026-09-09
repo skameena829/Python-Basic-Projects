d77a8433369b27db66a2e5d4640c9b11e6a62a53
skameena829 <skameena829@gmail.com>
2026-09-09 16:02:21 +0530

    Update

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,20 @@
-a = int(input())
-b = int(input())
+a = int(input("Enter a Value: "))
+b = int(input("Enter b Value: "))
 
-print(a+b)
+# Addition 
+add = a + b
+print(f"Addition of a and b is : {add}")
+
+# Subtration
+sub = a - b
+print(f"Subtraction of a and b is : {sub}")
+
+# Multiplication
+Mul = a * b
+print(f"Multiplication of a and b is : {Mul}")
+
+# Division
+Div = a//b
+print(f"Division of a and b is : {Div}")
+
+
